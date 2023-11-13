447bde26c4d037c14d6f62e50d3eeeff37674a66
geumhyunlee <goldbutnew@gmail.com>
2023-11-13 17:34:04 +0900

    solved

---
+++ 15989_123_더하기_4.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+# n은 10000보다 작거나 같으니까 dp의 길이를 10001로 설정
+# 모든 숫자가 1만 사용하는 경우의 수를 1가씩 보유하고 있으므로 1로 초기화
+dp = [1] * 10001
+
+# 2가 들어가는 경우
+for i in range(2, 10001):
+    dp[i] += dp[i - 2]
+
+# 3이 들어가는 경우
+for i in range(3, 10001):
+    dp[i] += dp[i - 3]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
